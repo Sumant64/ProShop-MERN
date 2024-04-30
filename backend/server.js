1f98349528572be1becb5a10e3167a1e59fb66ed
Sumant Shah <sumant.shah@gigacars.com>
2024-04-30 11:45:03 +0530

    model definition

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 app.get('/', (req, res) => {
     res.send('api is running...')
 })
