aa5366a933c676bea4b0b9d488f61b44aa3c5671
Sumant Shah <sumant.shah@gigacars.com>
2024-05-02 16:52:31 +0530

    get product, asyncHandler & errorMiddleware integration

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { getProducts } from '../controllers/productController.js';
+
+
+const router = express.Router();
+
+router.route('/').get(getProducts);
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -11,10 +14,16 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use('/api/products', productRoutes);
 
 
-app.get('/', (req, res) => {
-    res.send('api is running...')
-})
+// app.get('/', (req, res) => {
+//     res.send('api is running...')
+// })
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`app is running in port ${PORT}`))
