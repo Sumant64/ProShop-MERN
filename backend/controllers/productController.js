168ab9bc92bb26025c57b1684dfb6408ce93093e
Sumant Shah <sumant.shah@gigacars.com>
2024-05-02 17:10:50 +0530

    get products search by name, brand, category, description modified

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,13 +9,24 @@ const getProducts = asyncHandler(async (req, res) => {
     const pageSize = process.env.PAGINATION_LIMIT;
     const page = Number(req.query.pageNumber) || 1;
     
-    const keyword = req.query.keyword ?
-        {
-            name: {
-                $regex: req.query.keyword,
-                $options: 'i'
-            }
-        } : {};
+    // to find only in name
+    // const keyword = req.query.keyword ?
+    //     {
+    //         name: {
+    //             $regex: req.query.keyword,
+    //             $options: 'i'
+    //         }
+    //     } : {};
+
+    // to find in multiple keys: name, brand, category, description
+    const keyword = req.query.keyword ? {
+        $or: [
+            {name: { $regex: req.query.keyword, $options: 'i'}},
+            {brand: { $regex: req.query.keyword, $options: 'i'}},
+            {category: { $regex: req.query.keyword, $options: 'i'}},
+            {description: { $regex: req.query.keyword, $options: 'i'}}
+        ]
+    } : {}
 
     const count = await Product.countDocuments({ ...keyword });
     const products = await Product.find({ ...keyword })
