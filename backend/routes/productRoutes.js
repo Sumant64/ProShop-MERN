328efa149f06afc5003055f5c1fdaa03003b683b
Sumant Shah <sumant.shah@gigacars.com>
2024-05-03 04:07:42 +0530

    (backend) auth middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getProductById, getProducts } from '../controllers/productController.js';
 import checkObjectId from '../middleware/checkObjectId.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 
 const router = express.Router();
@@ -8,7 +9,7 @@ const router = express.Router();
 router.route('/').get(getProducts);
 
 
-router.route('/:id').get(checkObjectId, getProductById)
+router.route('/:id').get(checkObjectId, protect, getProductById)
 
 
 export default router;
